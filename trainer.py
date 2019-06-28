e1c382dbe370f6a658914254e6b3753b9cce1803
Divyansh Saxena <34120560+divyansh375@users.noreply.github.com>
2019-06-28 00:13:01 -0700

    Add files via upload

---
+++ trainer.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+import requests
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
+
+url = "http://192.168.0.116:8080//shot.jpg"
+c=0
+while (c<50):
+
+
+    img_resp = requests.get(url)
+    img_arr = np.array(bytearray(img_resp.content) , dtype = np.uint8)
+    img = cv2.imdecode(img_arr , -1)
+  
+     
+    
+  
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+  
+    
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+
+    for (x,y,w,h) in faces: 
+        
+         
+        roi_gray = gray[y:y+h, x:x+w] 
+        roi_color = img[y:y+h, x:x+w]
+        resize_img = cv2.resize(roi_gray  , (192 , 192))
+        s=""
+        s= s + str(c)
+        s=s+".jpg"
+        cv2.imwrite(s,resize_img)
+    c=c+1
+
+
+
+    
+
+        
